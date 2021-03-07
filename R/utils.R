192238f502559addfe5b4c403136d262499d492d
VP Nagraj <nagraj@nagraj.net>
2021-03-07 11:25:24 -0500

    abstracting kable of twoxtwo obj to display() helper

--- R/utils.R
+++ R/utils.R
@@ -44,3 +44,41 @@ format_measure <- function(.data, digits = 3) {
          round(tmp_upper, digits),
          ")")
 }
+
+
+#' Title
+#'
+#' @param .twoxtwo
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+display <- function(.twoxtwo, ...) {
+  ## extract exposure and outcome names / levels from twoxtwo object
+  exposure_var <- .twoxtwo$exposure$variable
+  exposure_levels <- .twoxtwo$exposure$levels
+  exposures <- paste0(exposure_var, "=", exposure_levels)
+
+  outcome_var <- .twoxtwo$outcome$variable
+  outcome_levels <- .twoxtwo$outcome$levels
+  outcomes <- paste0(outcome_var, "=", outcome_levels)
+
+  ## stack names of outcomes with the first two columns of the twoxtwo tibble
+  tmp <- rbind(outcomes, .twoxtwo$tbl[,1:2])
+
+  tmp <-
+    cbind(c("", exposures),tmp) %>%
+    as.matrix(.)
+
+  rownames(tmp) <- c("","EXPOSURE","EXPOSURE")
+  names(tmp) <- NULL
+
+  knitr::kable(tmp,
+               col.names = c("","OUTCOME","OUTCOME"),
+               row.names = TRUE,
+               ...)
+
+}
+
