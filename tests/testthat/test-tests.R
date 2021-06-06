ea2ee2c9288576b8f9e6605739c7130a84bb65eb
VP Nagraj <nagraj@nagraj.net>
2021-06-06 19:09:25 -0400

    adding test for chisq() and cleaning up fisher() test

--- tests/testthat/test-tests.R
+++ tests/testthat/test-tests.R
@@ -2,8 +2,19 @@ test_that("fisher test p-value comes through as expected", {
 
   tmp_fisher_res <-
     titanic %>%
-    dplyr::mutate(Crew = ifelse(Class == "Crew", TRUE, FALSE)) %>%
     fisher(., Crew, Survived)
 
+  ## should be p < 0.05
   expect_lt(tmp_fisher_res$pvalue, 0.05)
 })
+
+test_that("chisq test p-value comes through as expected", {
+
+  tmp_chisq_res <-
+    titanic %>%
+    chisq(., Crew, Survived)
+
+  ## should be p < 0.05
+  expect_lt(tmp_chisq_res$pvalue, 0.05)
+})
+
